2b40fb462a5207a3912b6d9aef2bac73982f4329
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2024-12-02 08:50:24 +0100

    försöker utöka

--- src/Consumable.java
+++ src/Consumable.java
@@ -1,7 +1,13 @@
 public class Consumable extends Item {
 public int usesMax = 0;
 public int usesCurrent = 0;
+
 public void use(Character Target){
+    if (usesCurrent < usesMax){
+        this.usesMax = usesMax;
+        this.name = name;
+    }
+
 
 }
 }
