6164fa216e4a5956a31e8c38d27cefbe841dfefd
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2024-12-02 09:06:00 +0100

    funktioner

--- src/Consumable.java
+++ src/Consumable.java
@@ -2,10 +2,14 @@ public class Consumable extends Item {
 public int usesMax = 0;
 public int usesCurrent = 0;
 
+public Consumable(int usesMax, String name){
+    this.usesMax = usesMax;
+    this.name = name;
+}
+
 public void use(Character Target){
     if (usesCurrent < usesMax){
-        this.usesMax = usesMax;
-        this.name = name;
+        System.out.println();
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner s = new Scanner(System.in);
+        Character mike = new Character();
 
+        mike.backpack.Display();
+        String ans = s.nextLine();
+        if (ans.equals("Yes")){
+            mike.backpack.items.add(new Armor("skinbidi armor", 42));
+            System.out.println(mike.name + " More items in inventory");
+            mike.backpack.Display();
 
+        }
+        else {
+            System.out.println(mike.name + " Didnt pick up item");
 
     }
 }
